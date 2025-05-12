8bb55321c48a5edbdb5ba06e47ded1e4639fcbb4
Etienne Le Goater <etiennelegoater@Etiennes-MacBook-Air.local>
2025-05-12 11:35:18 +0100

    test

--- content.js
+++ content.js
@@ -9,7 +9,7 @@ chrome.storage.sync.get(['zoomEnabled'], (result) => {
     toggle.checked = result.zoomEnabled ?? true;
   });
 
-// Listen for toggle messages from 
+// Listen for toggle messages from popup
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     console.log('Received message:', message);
     if (message.type === 'TOGGLE_ZOOM') {
